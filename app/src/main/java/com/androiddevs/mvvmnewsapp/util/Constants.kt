60e9d34f356f311771afb962391f531968f619d7
Hattapong <aumdarkness@gmail.com>
2023-11-09 17:31:42 +0700

    eos:: 7

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "f783910672bd48e891239971d9f826e7"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
